884032dfa4cc71c59c1d3937bf513b3eeba42d65
DrewManson <dmanson@umich.edu>
2019-05-26 21:05:43 -0400

    Fixing my mistakes

--- include/engine/events/event.hpp
+++ include/engine/events/event.hpp
@@ -28,16 +28,16 @@ enum class MouseButton
 // match the ascii value if applicable
 enum class Keyboard
 {
-	UNKNOWN = -1,
+    UNKNOWN = -1,
 
-	A = 65,  B, C, D, E, F, G, H, I, J,
-	K, L, M, N, O, P, Q, R, S, T,
-	U, V, W, X, Y, Z,
+    A = 65, B, C, D, E, F, G, H, I, J,
+    K, L, M, N, O, P, Q, R, S, T,
+    U, V, W, X, Y, Z,
 
-	a = 97, b, c, d, e, f, g, h, i, j,
-	k, l, m, n, o, p, q, r, s, t,
-	u, v, w, x, y, z
+    a = 97, b, c, d, e, f, g, h, i, j,
+    k, l, m, n, o, p, q, r, s, t,
+    u, v, w, x, y, z
 
-}
+};
 
 }
